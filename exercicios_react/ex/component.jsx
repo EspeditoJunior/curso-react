fe5b322647837fc26e4f3c41a084266660cb5a5f
EspeditoJunior <e.jr42@hotmail.com>
2018-01-27 14:31:20 -0200

    #aula 28

--- exercicios_react/ex/component.jsx
+++ exercicios_react/ex/component.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro componente!</h1>
-}
+export const Primeiro =  props => (
+    <h1>Primeiro</h1>
+)
+
+export const Segundo =  props => (
+    <h1>Segundo</h1>
+)
--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Component from './component'
+import {Primeiro,Segundo} from './component'
+import SilvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
 
-ReactDOM.render(<Component/>, document.getElementById('app'))
+ReactDOM.render(
+    <Family lastName="Nascimento">
+        <Member name="Espedito"/>
+        <Member name="Jana"/>
+    </Family>
+    , document.getElementById('app')
+)
+
+/*
+ReactDOM.render(
+    <div>
+        <Primeiro/>
+        <Segundo/>
+    </div>
+ 
+, document.getElementById('app'))
+*/
